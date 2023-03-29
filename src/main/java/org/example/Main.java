7149fde212084caebfc3fc548b711e4dbe8585e9
Programa-creator <116491894+Programa-creator@users.noreply.github.com>
2023-03-29 15:44:23 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,21 +4,18 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        // Створення об'єкту Homework та збереження у базі даних
+
         Homework homework1 = new Homework("Домашня робота з математики", "Виконати завдання №1, №2 та №3 на сторінці 23");
         HomeworkDao homeworkDao = new HomeworkDao();
         homeworkDao.addHomework(homework1);
 
-        // Створення об'єкту Lesson та збереження у базі даних
         Lesson lesson1 = new Lesson("Математика", homework1.getId());
         LessonDao lessonDao = new LessonDao();
         lessonDao.addLesson(lesson1);
 
-        // Оновлення інформації про домашню роботу
         homework1.setDescription("Виконати завдання №1, №2, №3 та №4 на сторінці 23");
         homeworkDao.updateHomework(homework1);
 
-        // Отримання всіх уроків та пов'язаних з ними домашніх робіт
         List<Lesson> lessons = lessonDao.getAllLessons();
         for (Lesson lesson : lessons) {
             Homework homework = homeworkDao.getHomeworkById(lesson.getHomeworkId());
@@ -28,7 +25,6 @@ public class Main {
             System.out.println();
         }
 
-        // Пошук домашньої роботи за назвою
         List<Homework> homeworkList = homeworkDao.getHomeworkByName("математика");
         for (Homework homework : homeworkList) {
             System.out.println("Назва домашньої роботи: " + homework.getName());
@@ -36,7 +32,6 @@ public class Main {
             System.out.println();
         }
 
-        // Видалення уроку та пов'язаної з ним домашньої роботи
         lessonDao.deleteLesson(lesson1.getId());
         homeworkDao.deleteHomework(homework1.getId());
     }
